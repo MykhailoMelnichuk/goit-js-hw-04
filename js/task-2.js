75543479eb68032a1a9f5f3f3da11753c4c2a841
MykhailoMelnichuk <mmdcomp@ukr.net>
2019-11-06 16:05:00 +0200

    Module-4 HW 1-2

---
+++ js/task-2.js
@@ -0,0 +1,27 @@
+console.log('Задание 2');
+
+const inventory = {
+    items: ['Монорельса', 'Фильтр'],
+    add(itemName) {
+      this.items.push(itemName);
+    },
+    remove(itemName) {
+      this.items = this.items.filter(item => item !== itemName);
+    },
+  };
+  
+  const invokeInventoryAction = function(itemName, action) {
+    console.log(`Invoking ${action.name} opeartion on ${itemName}`);
+    action.call(inventory, itemName);
+  };
+  
+  invokeInventoryAction('Аптечка', inventory.add);
+  // Invoking add opeartion on Аптечка
+  
+  console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+  
+  invokeInventoryAction('Фильтр', inventory.remove);
+  // Invoking remove opeartion on Фильтр
+  
+  console.log(inventory.items); // ['Монорельса', 'Аптечка']
+  
